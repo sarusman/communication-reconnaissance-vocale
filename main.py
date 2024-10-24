f89bff610cb6d62d8651d33d8f8dfd13e9536ebe
Sarusman <sarusmandu92@gmail.com>
2024-10-24 09:40:35 +0200

    Fichier de reconnaissance Vocale

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 
-# Créer la fenêtre principale
+
 fenetre = tk.Tk()
 
-# Définir le titre de la fenêtre
+
 fenetre.title("Fenêtre Vide")
 
-# Définir la taille de la fenêtre (optionnel)
+
 fenetre.geometry("400x300")
 
-# Démarrer la boucle principale de Tkinter
+
 fenetre.mainloop()
